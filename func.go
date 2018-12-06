db5ccbf692921bef7fccc75156f96cbdfce8dda7
xiang <william.wu@shein.com>
2018-12-06 15:14:00 +0800

    'edit'

--- func.go
+++ func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"reflect"
+)
 
 func eval(a,b int ,op string) int  {
 	switch op {
@@ -16,16 +21,48 @@ func eval(a,b int ,op string) int  {
 		panic("not support "+op)
 	}
 }
+
+func div(a,b int)(int,int){
+	return a/b,a%b
+}
+
+func div2(a,b int)(int ,error){
+	return a/b,nil
+}
+
+func apply(op func(int,int) int ,a,b int) int  {
+	fmt.Printf("calling %s with %d,%d\n",runtime.FuncForPC(reflect.ValueOf(op).Pointer()).Name(),a,b)
+	return op(a,b)
+}
+
+func sum(numbers... int) int  {
+	var s = 0;
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
 func main()  {
+	fmt.Println(sum(1,2,3,4,5))
+
 	res := eval(3,4,"+")
 	fmt.Println(res)
 
-	res1 := eval(3,4,"*")
-	fmt.Println(res1)
+	a,b := div(13,4)
+	fmt.Printf("13除以4等于，商%d,余数%d \n",a,b)
 
-	res2 := eval(1,3,"/")
-	fmt.Println(res2)
+	c,_ := div2(13,3)
+	fmt.Printf("13除以3等于，商%d,余数%d \n",c)
 
-	res3 := eval(1,3,"-")
-	fmt.Println(res3)
+	//if e,err := div2(13,0); err!=nil{
+	//	panic(err)
+	//}else {
+	//	fmt.Println(e)
+	//}
+
+	fmt.Println(apply(
+		func(a int, b int) int {
+			return int(math.Pow(float64(a),float64(b)))
+		},3,4))
+
 }
